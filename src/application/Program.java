6f2fe32a5ed29dd89499909c67581bad524031c5
Yuri Matthewus Sousa de Oliveira <yuri_matteus@hotmail.com>
2023-02-03 14:59:57 -0300

    Chess layer and printing board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
-		Position position = new Position (6,9);
-		
-		System.out.println(position);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }

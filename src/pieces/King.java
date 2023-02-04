743e11b435d0f122ca8c9ec49e5c69c1003e6813
Yuri Matthewus Sousa de Oliveira <yuri_matteus@hotmail.com>
2023-02-03 21:13:24 -0300

    Placing pieces on the board

---
+++ src/pieces/King.java
@@ -0,0 +1,21 @@
+package pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class King extends ChessPiece {
+	
+	public King () {
+		
+	}
+	
+	public King (Board board, Color color) {
+		super (board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
+	}
+}
---
+++ src/pieces/Rook.java
@@ -0,0 +1,21 @@
+package pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+
+	public Rook () {
+		
+	}
+	
+	public Rook(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "R";
+	}
+}

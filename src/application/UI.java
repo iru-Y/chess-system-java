31f269eacdbc0de935e4cc4ab9e6b1e007802df6
Yuri Matthewus Sousa de Oliveira <yuri_matteus@hotmail.com>
2023-02-04 23:39:55 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -53,11 +53,20 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
-		
-		
-		
+		System.out.println("  a b c d e f g h");		
 	}
+	
+	public static void printBoard(ChessPiece[][] pieces , boolean [][] possibleMoves) {
+		for (int i = 0 ; i < pieces.length ; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0 ; j < pieces.length ; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);	
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");		
+	}
+	
 	
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if (background) {
